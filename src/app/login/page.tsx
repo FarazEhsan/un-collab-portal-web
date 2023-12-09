d3e6bcc587d4a108ace71035e1d0befe01ba77be
Abdullah Shafiq <abdullahshafiq377@gmail.com>
2023-12-09 23:54:24 +0500

    Login Page Ready

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,8 @@
+import LoginForm from "@/app/login/login-form";
+import Link from "next/link";
+
 export default function LoginPage() {
+
     return (
         <>
             <div className="flex min-h-full flex-1">
@@ -16,89 +20,16 @@ export default function LoginPage() {
                             </h2>
                             <p className="mt-2 text-sm leading-6 text-gray-500">
                                 Not a member?{' '}
-                                <a href="#"
+                                <Link href='/register'
                                    className="font-semibold text-custom-blue hover:text-custom-teal">
                                     Register Now
-                                </a>
+                                </Link>
                             </p>
                         </div>
 
                         <div className="mt-10">
                             <div>
-                                <form className="space-y-6">
-                                    <div>
-                                        <label htmlFor="email"
-                                               className="block text-sm font-medium leading-6 text-gray-900 dark:text-gray-100">
-                                            Email address
-                                        </label>
-                                        <div className="mt-2">
-                                            <input
-                                                id="email"
-                                                name="email"
-                                                type="email"
-                                                autoComplete="email"
-                                                required
-                                                className="block w-full rounded-md
-                                                border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400
-                                                focus:ring-2 focus:ring-inset focus:ring-custom-teal sm:text-sm sm:leading-6
-                                                dark:bg-gray-800 dark:ring-gray-700 dark:focus:ring-custom-teal"
-                                            />
-                                        </div>
-                                    </div>
-
-                                    <div>
-                                        <label htmlFor="password"
-                                               className="block text-sm font-medium leading-6 text-gray-900 dark:text-gray-100">
-                                            Password
-                                        </label>
-                                        <div className="mt-2">
-                                            <input
-                                                id="password"
-                                                name="password"
-                                                type="password"
-                                                autoComplete="current-password"
-                                                required
-                                                className="block w-full rounded-md border-0 py-1.5 shadow-sm
-                                                ring-1 ring-inset ring-gray-300 placeholder:text-gray-400
-                                                focus:ring-2 focus:ring-inset focus:ring-custom-teal sm:text-sm sm:leading-6
-                                                dark:bg-gray-800 dark:ring-gray-700 dark:focus:ring-custom-teal"
-                                            />
-                                        </div>
-                                    </div>
-
-                                    <div
-                                        className="flex items-center justify-between">
-                                        <div className="flex items-center">
-                                            <input
-                                                id="remember-me"
-                                                name="remember-me"
-                                                type="checkbox"
-                                                className="h-4 w-4 rounded border-gray-300 text-custom-teal focus:ring-custom-teal
-                                                dark:bg-gray-800 dark:border-gray-700 dark:text-custom-teal dark:focus:ring-custom-teal"
-                                            />
-                                            <label htmlFor="remember-me"
-                                                   className="ml-3 block text-sm leading-6 text-gray-700 dark:text-gray-400">
-                                                Remember me
-                                            </label>
-                                        </div>
-
-                                        <div className="text-sm leading-6">
-                                            <a href="#"
-                                               className="font-semibold text-custom-blue hover:text-custom-teal">
-                                                Forgot password?
-                                            </a>
-                                        </div>
-                                    </div>
-
-                                    <div>
-                                        <button
-                                            type="submit"
-                                            className="flex w-full justify-center rounded-md bg-custom-blue px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-custom-teal focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-custom-blue"
-                                        >
-                                            Sign in
-                                        </button>
-                                    </div>
-                                </form>
+                                <LoginForm/>
                             </div>
 
                             {/*<div className="mt-10">*/}
