9774482fddade31c50781086c569b72454cd672b
Faraz Ehsan <faraz.ahsaan@hotmail.com>
2023-12-17 14:51:59 +0500

    User role based login

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,35 @@
-import { withMiddlewareAuthRequired } from '@auth0/nextjs-auth0/edge';
+import { UserProfile } from '@auth0/nextjs-auth0/client';
+import { Session, getSession, withMiddlewareAuthRequired } from '@auth0/nextjs-auth0/edge';
+import { NextResponse } from 'next/server';
 
-export default withMiddlewareAuthRequired();
+// Define a type for the roles
+type Role = 'admin' | 'user';
 
-// export const config = {
-//     matcher: '/about/:path*'
-//   };
+// Define a mapping of roles to paths
+const rolePaths: Record<Role, string[]> = {
+  admin: ['/register', '/admin', '/profile'],
+  user: ['/dashboard', '/profile'],
+  // Add more roles and paths as needed
+};
+
+// Define a type for the user object that includes the roles property
+type UserWithRoles = UserProfile & {
+  'https://unhabitat-cop.com/roles': Role[];
+};
+
+export default withMiddlewareAuthRequired(async function middleware(req) {
+  const res = NextResponse.next();
+  const session = await getSession(req, res) as Session;
+  const user = session.user as UserWithRoles;
+  const roles = user['https://unhabitat-cop.com/roles'];
+  const requestedPath = req.nextUrl.pathname;
+
+  // Check if the user's roles allow them to access the requested path
+  const canAccess = roles.some(role => rolePaths[role]?.includes(requestedPath));
+
+  if (canAccess) {
+    return res;
+  } else {
+    return NextResponse.redirect(new URL("/dashboard", req.url));
+  }
+});
