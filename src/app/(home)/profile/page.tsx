1bf10e6a07796ff0f3a1a549ce7556cb20e2eba1
Abdullah Shafiq <abdullahshafiq377@gmail.com>
2023-12-20 23:12:29 +0500

    Slide Overs Added
    Edit Personal Info Slide Over, Edit Contact Info Slide Over and Project Details Slide Over Added

--- src/app/(home)/profile/page.tsx
+++ src/app/(home)/profile/page.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
-import SingleColumnContainer from "@/components/navigation/singleColumnContainer";
+'use client'
+import React, {useState} from 'react';
+import SingleColumnContainer
+    from "@/components/navigation/singleColumnContainer";
 import RoundButton from "@/components/button/round-button";
 import ProjectCard from "@/components/cards/project-card";
+import Button from "@/components/button/Button";
+import EditPersonalInfoSlideOver
+    from "@/app/(home)/profile/edit-personal-info-slide-over";
+import EditContactInfoSlideOver
+    from "@/app/(home)/profile/edit-contact-info-slide-over";
 
 const ProfilePage = () => {
+    const [openEditPersonalInfoSlideOver, setOpenEditPersonalInfoSlideOver] = useState(false)
+    const [openEditContactInfoSlideOver, setOpenEditContactInfoSlideOver] = useState(false)
     return (
         <SingleColumnContainer>
-            <div className="mx-auto max-w-2xl space-y-16 sm:space-y-20 lg:mx-0 lg:max-w-none">
+            <div
+                className="mx-auto max-w-2xl space-y-16 sm:space-y-20 lg:mx-0 lg:max-w-none">
                 <div>
                     <div className="flex flex-col items-center">
                         <img
@@ -21,7 +31,8 @@ const ProfilePage = () => {
                             className="text-gray-900 dark:text-gray-100">He/Him
                         </p>
                         <div className="mt-2">
-                            <RoundButton classNames="mx-1 bg-[#0866ff] hover:bg-[#478cfc]">
+                            <RoundButton
+                                classNames="mx-1 bg-[#0866ff] hover:bg-[#478cfc]">
                                 {/*Facebook*/}
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
@@ -32,7 +43,8 @@ const ProfilePage = () => {
                                         d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"/>
                                 </svg>
                             </RoundButton>
-                            <RoundButton classNames="mx-1 bg-[#d62976] hover:bg-[#cf4e87]">
+                            <RoundButton
+                                classNames="mx-1 bg-[#d62976] hover:bg-[#cf4e87]">
                                 {/*Instagram*/}
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
@@ -43,7 +55,8 @@ const ProfilePage = () => {
                                         d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"/>
                                 </svg>
                             </RoundButton>
-                            <RoundButton classNames="mx-1 bg-[#0077b5] hover:bg-[#3d8ab3]">
+                            <RoundButton
+                                classNames="mx-1 bg-[#0077b5] hover:bg-[#3d8ab3]">
                                 {/*Linkedin*/}
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
@@ -54,7 +67,8 @@ const ProfilePage = () => {
                                         d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z"/>
                                 </svg>
                             </RoundButton>
-                            <RoundButton classNames="mx-1 bg-[#000] hover:bg-[#4f4e4e]">
+                            <RoundButton
+                                classNames="mx-1 bg-[#000] hover:bg-[#4f4e4e]">
                                 {/*Tiktok*/}
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
@@ -65,7 +79,8 @@ const ProfilePage = () => {
                                         d="M448,209.91a210.06,210.06,0,0,1-122.77-39.25V349.38A162.55,162.55,0,1,1,185,188.31V278.2a74.62,74.62,0,1,0,52.23,71.18V0l88,0a121.18,121.18,0,0,0,1.86,22.17h0A122.18,122.18,0,0,0,381,102.39a121.43,121.43,0,0,0,67,20.14Z"/>
                                 </svg>
                             </RoundButton>
-                            <RoundButton classNames="mx-1 bg-[#1DA1F2] hover:bg-[#6bc4fa]">
+                            <RoundButton
+                                classNames="mx-1 bg-[#1DA1F2] hover:bg-[#6bc4fa]">
                                 {/*Twitter*/}
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
@@ -76,7 +91,8 @@ const ProfilePage = () => {
                                         d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"/>
                                 </svg>
                             </RoundButton>
-                            <RoundButton classNames="mx-1 bg-[#25d366] hover:bg-[#69cf8f]">
+                            <RoundButton
+                                classNames="mx-1 bg-[#25d366] hover:bg-[#69cf8f]">
                                 {/*Whatsapp*/}
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
@@ -91,13 +107,21 @@ const ProfilePage = () => {
                     </div>
                 </div>
                 <div>
-                    <h2 className="text-lg font-semibold leading-7 text-gray-900 dark:text-gray-100">
-                        Personal Info
-                    </h2>
-                    <p className="mt-1 text-sm leading-6 text-gray-500 ">
-                        This information will be displayed publicly so be
-                        careful what you share.
-                    </p>
+                    <div className="flex flex-row items-center">
+                        <div>
+                            <h2 className="text-lg font-semibold leading-7 text-gray-900 dark:text-gray-100">
+                                Personal Info
+                            </h2>
+                            <p className="mt-1 text-sm leading-6 text-gray-500 ">
+                                This information will be displayed publicly so
+                                be
+                                careful what you share.
+                            </p>
+                        </div>
+                        <Button
+                            onClick={() => setOpenEditPersonalInfoSlideOver(!openEditPersonalInfoSlideOver)}
+                            colorType="link" classNames="ml-auto">Edit</Button>
+                    </div>
                     <dl className="mt-4 space-y-6 divide-y divide-gray-100 dark:divide-gray-800 border-t border-gray-200 dark:border-gray-700 text-sm leading-6">
                         {/*<div className="pt-6 sm:flex">*/}
                         {/*    <dt className="font-medium text-gray-900 dark:text-gray-100 sm:w-64 sm:flex-none sm:pr-6">Name</dt>*/}
@@ -114,19 +138,25 @@ const ProfilePage = () => {
                         <div className="pt-6 sm:flex">
                             <dt className="font-medium text-gray-900 dark:text-gray-100 sm:w-64 sm:flex-none sm:pr-6">City</dt>
                             <dd className="mt-1 flex justify-between gap-x-6 sm:mt-0 sm:flex-auto">
-                                <div className="text-gray-900 dark:text-gray-100">California</div>
+                                <div
+                                    className="text-gray-900 dark:text-gray-100">California
+                                </div>
                             </dd>
                         </div>
                         <div className="pt-6 sm:flex">
                             <dt className="font-medium text-gray-900 dark:text-gray-100 sm:w-64 sm:flex-none sm:pr-6">Country</dt>
                             <dd className="mt-1 flex justify-between gap-x-6 sm:mt-0 sm:flex-auto">
-                                <div className="text-gray-900 dark:text-gray-100">USA</div>
+                                <div
+                                    className="text-gray-900 dark:text-gray-100">USA
+                                </div>
                             </dd>
                         </div>
                         <div className="pt-6 sm:flex">
                             <dt className="font-medium text-gray-900 dark:text-gray-100 sm:w-64 sm:flex-none sm:pr-6">Age</dt>
                             <dd className="mt-1 flex justify-between gap-x-6 sm:mt-0 sm:flex-auto">
-                                <div className="text-gray-900 dark:text-gray-100">50</div>
+                                <div
+                                    className="text-gray-900 dark:text-gray-100">50
+                                </div>
                             </dd>
                         </div>
                         {/*<div className="pt-6 sm:flex">*/}
@@ -138,23 +168,36 @@ const ProfilePage = () => {
                     </dl>
                 </div>
                 <div>
-                    <h2 className="text-lg font-semibold leading-7 text-gray-900 dark:text-gray-100">
-                        Contact Info
-                    </h2>
-                    <p className="mt-1 text-sm leading-6 text-gray-500 ">
-                        This information will be displayed publicly so be careful what you share.
-                    </p>
+                    <div className="flex flex-row items-center">
+                        <div>
+                            <h2 className="text-lg font-semibold leading-7 text-gray-900 dark:text-gray-100">
+                                Contact Info
+                            </h2>
+                            <p className="mt-1 text-sm leading-6 text-gray-500 ">
+                                This information will be displayed publicly so
+                                be
+                                careful what you share.
+                            </p>
+                        </div>
+                        <Button
+                            onClick={() => setOpenEditContactInfoSlideOver(!openEditContactInfoSlideOver)}
+                            colorType="link" classNames="ml-auto">Edit</Button>
+                    </div>
                     <dl className="mt-4 space-y-6 divide-y divide-gray-100 dark:divide-gray-800 border-t border-gray-200 dark:border-gray-700 text-sm leading-6">
                         <div className="pt-6 sm:flex">
                             <dt className="font-medium text-gray-900 dark:text-gray-100 sm:w-64 sm:flex-none sm:pr-6">Email</dt>
                             <dd className="mt-1 flex justify-between gap-x-6 sm:mt-0 sm:flex-auto">
-                                <div className="text-gray-900 dark:text-gray-100">tom.cook@example.com</div>
+                                <div
+                                    className="text-gray-900 dark:text-gray-100">tom.cook@example.com
+                                </div>
                             </dd>
                         </div>
                         <div className="pt-6 sm:flex">
                             <dt className="font-medium text-gray-900 dark:text-gray-100 sm:w-64 sm:flex-none sm:pr-6">Phone/Whatsapp</dt>
                             <dd className="mt-1 flex justify-between gap-x-6 sm:mt-0 sm:flex-auto">
-                                <div className="text-gray-900 dark:text-gray-100">0123456778</div>
+                                <div
+                                    className="text-gray-900 dark:text-gray-100">0123456778
+                                </div>
                             </dd>
                         </div>
                     </dl>
@@ -200,33 +243,58 @@ const ProfilePage = () => {
                 {/*    </dl>*/}
                 {/*</div>*/}
                 <div>
-                    <h2 className="text-lg font-semibold leading-7 text-gray-900 dark:text-gray-100">
-                        Groups
-                    </h2>
-                    <p className="mt-1 text-sm leading-6 text-gray-500 ">
-                        This information will be displayed publicly so be careful what you share.
-                    </p>
+                    <div className="flex flex-row items-center">
+                        <div>
+                            <h2 className="text-lg font-semibold leading-7 text-gray-900 dark:text-gray-100">
+                                Groups
+                            </h2>
+                            <p className="mt-1 text-sm leading-6 text-gray-500 ">
+                                This information will be displayed publicly so
+                                be
+                                careful what you share.
+                            </p>
+                        </div>
+                        <Button
+                            colorType="link" classNames="ml-auto">Edit</Button>
+                    </div>
+
                     <dl className="mt-4 space-y-6 divide-y divide-gray-100 dark:divide-gray-800 border-t border-gray-200 dark:border-gray-700 text-sm leading-6">
                         <div className="pt-6 sm:flex">
-                            <dt className="font-medium text-gray-900 dark:text-gray-100 sm:w-64 sm:flex-none sm:pr-6">Primary Groups</dt>
+                            <dt className="font-medium text-gray-900 dark:text-gray-100 sm:w-64 sm:flex-none sm:pr-6">Primary
+                                Groups
+                            </dt>
                             <dd className="mt-1 flex justify-between gap-x-6 sm:mt-0 sm:flex-auto">
                                 <ul>
-                                <li className="text-gray-900 dark:text-gray-100">Youth Advisory Board</li>
-                                <li className="text-gray-900 dark:text-gray-100">Youth 2030 Cities Alumni</li>
-                                <li className="text-gray-900 dark:text-gray-100">Young Gamechangers</li>
+                                    <li className="text-gray-900 dark:text-gray-100">Youth
+                                        Advisory Board
+                                    </li>
+                                    <li className="text-gray-900 dark:text-gray-100">Youth
+                                        2030 Cities Alumni
+                                    </li>
+                                    <li className="text-gray-900 dark:text-gray-100">Young
+                                        Gamechangers
+                                    </li>
                                 </ul>
                             </dd>
                         </div>
                     </dl>
                 </div>
                 <div>
-                    <h2 className="text-lg font-semibold leading-7 text-gray-900 dark:text-gray-100">
-                        Highlights
-                    </h2>
-                    <p className="mt-1 text-sm leading-6 text-gray-500 ">
-                        This information will be displayed publicly so be
-                        careful what you share.
-                    </p>
+                    <div className="flex flex-row items-center">
+                        <div>
+                            <h2 className="text-lg font-semibold leading-7 text-gray-900 dark:text-gray-100">
+                                Highlights
+                            </h2>
+                            <p className="mt-1 text-sm leading-6 text-gray-500 ">
+                                This information will be displayed publicly so
+                                be
+                                careful what you share.
+                            </p>
+                        </div>
+                        <Button
+                            colorType="link" classNames="ml-auto">Edit</Button>
+                    </div>
+
                     <div className="grid grid-cols-3 gap-4 mt-4">
                         <ProjectCard/>
                         <ProjectCard/>
@@ -288,8 +356,11 @@ const ProfilePage = () => {
                     {/*    </div>*/}
                     {/*</dl>*/}
                 </div>
-
             </div>
+            <EditPersonalInfoSlideOver open={openEditPersonalInfoSlideOver}
+                                       setOpen={setOpenEditPersonalInfoSlideOver}/>
+            <EditContactInfoSlideOver open={openEditContactInfoSlideOver}
+                                      setOpen={setOpenEditContactInfoSlideOver}/>
         </SingleColumnContainer>
     );
 };
