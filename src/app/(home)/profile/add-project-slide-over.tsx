5f93db07052697d1352644aa4fa37a3baa882f48
Abdullah Shafiq <abdullahshafiq377@gmail.com>
2023-12-28 10:05:35 +0500

    Combobox updated, added combobox to add project slideover

--- src/app/(home)/profile/add-project-slide-over.tsx
+++ src/app/(home)/profile/add-project-slide-over.tsx
@@ -1,16 +1,26 @@
-import React, {Fragment, SetStateAction} from 'react'
+import React, {Fragment, SetStateAction, useState} from 'react'
 import {Dialog, Transition} from '@headlessui/react'
 import {XMarkIcon} from '@heroicons/react/24/outline'
 import Input from "@/components/form/Input";
 import Button from "@/components/button/Button";
 import TextArea from "@/components/form/TextArea";
+import ComboBox from "@/components/form/combo-box";
 
 interface SlideOverProps {
     open: boolean,
     setOpen: React.Dispatch<SetStateAction<boolean>>
 }
 
+const sdgs = [
+    {id: 1, name: 'No poverty'},
+    {id: 2, name: 'Zero hunger'},
+    {id: 3, name: 'Good health and well-being'},
+    {id: 4, name: 'Quality education'},
+    {id: 5, name: 'Gender equality'},
+]
+
 export default function AddProjectSlideOver({open, setOpen}: SlideOverProps) {
+    const [selectedSdgs, setSelectedSdgs] = useState([]);
     return (
         <Transition.Root show={open} as={Fragment}>
             <Dialog as="div" className="relative z-50" onClose={setOpen}>
@@ -88,11 +98,18 @@ export default function AddProjectSlideOver({open, setOpen}: SlideOverProps) {
                                                            name="dateTo"
                                                            type="date"/>
                                                 </div>
-                                                {/*TODO: Implement Combobox for SDGs*/}
                                                 <div className="mt-4">
-                                                    <TextArea label="Description"
-                                                           name="description"
-                                                           placeholder="A short description of your project"/>
+                                                    <ComboBox multiple={true}
+                                                              items={sdgs}
+                                                              selectedData={selectedSdgs}
+                                                              setSelectedData={setSelectedSdgs}
+                                                              label='SDGs'/>
+                                                </div>
+                                                <div className="mt-4">
+                                                    <TextArea
+                                                        label="Description"
+                                                        name="description"
+                                                        placeholder="A short description of your project"/>
                                                 </div>
 
                                             </div>
@@ -101,7 +118,9 @@ export default function AddProjectSlideOver({open, setOpen}: SlideOverProps) {
                                             className="mt-4 flex-shrink-0 border-t border-gray-200 dark:border-gray-700 px-4 py-5 sm:px-6">
                                             <div
                                                 className="flex justify-end space-x-3">
-                                                <Button onClick={() => setOpen(false)} colorType="secondary">Cancel</Button>
+                                                <Button
+                                                    onClick={() => setOpen(false)}
+                                                    colorType="secondary">Cancel</Button>
                                                 <Button>Create</Button>
                                             </div>
                                         </div>
--- src/components/form/combo-box.tsx
+++ src/components/form/combo-box.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Combobox} from "@headlessui/react";
 import Badge from "@/components/badge";
 import {CheckIcon, ChevronUpDownIcon} from "@heroicons/react/24/outline";
@@ -6,9 +6,10 @@ import {classNames} from "@/utils/extraFunctions";
 
 
 
-const ComboBox = ({multiple, data, label}:any) => {
+const ComboBox = ({multiple, items, label, selectedData, setSelectedData}:any) => {
     const [query, setQuery] = useState('')
-    const [selectedData, setSelectedData] = useState([])
+    // const [selectedData, setSelectedData] = useState([]);
+    const data = items;
 
     const filteredData =
         query === ''
@@ -26,7 +27,7 @@ const ComboBox = ({multiple, data, label}:any) => {
             {multiple && selectedData?.length > 0 && (
                 <div className="grid grid-cols-3 gap-2">
                     {selectedData?.map((item:any) => (
-                        <Badge key={item.id} text={item.name}/>
+                        <Badge key={item.id} text={items.filter((d:any) => (d.id === item))[0]?.name}/>
                     ))}
                 </div>
             )}
@@ -34,7 +35,7 @@ const ComboBox = ({multiple, data, label}:any) => {
                 <Combobox.Input
                     className="w-full rounded-md border-0 bg-white py-1.5 pl-3 pr-10 text-gray-900 dark:text-gray-100 dark:ring-gray-600 dark:bg-gray-800 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-custom-teal sm:text-sm sm:leading-6"
                     onChange={(event) => setQuery(event.target.value)}
-                    displayValue={(item) => item?.name}
+                    displayValue={(item) => items.filter((d:any) => (d.id === item))[0]?.name}
                 />
                 <Combobox.Button className="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none">
                     <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
@@ -42,9 +43,10 @@ const ComboBox = ({multiple, data, label}:any) => {
                 {filteredData.length > 0 && (
                     <Combobox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white dark:bg-gray-800 py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
                         {filteredData.map((item:any) => (
+
                             <Combobox.Option
                                 key={item.id}
-                                value={item}
+                                value={item.id}
                                 className={({ active }) =>
                                     classNames(
                                         'relative cursor-default select-none py-2 pl-8 pr-4',
@@ -55,7 +57,6 @@ const ComboBox = ({multiple, data, label}:any) => {
                                 {({ active, selected }) => (
                                     <>
                                         <span className={classNames('block truncate', selected && 'font-semibold')}>{item.name}</span>
-
                                         {selected && (
                                             <span
                                                 className={classNames(
