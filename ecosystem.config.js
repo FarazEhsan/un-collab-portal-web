f35c096890a7eaff42c438b1127b8e3e67c77ccc
Faraz Ehsan <faraz.ahsaan@hotmail.com>
2024-01-10 19:28:18 +0500

    fixed port

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
       {
         name: "un-collab-portal-web",
         script: "./node_modules/next/dist/bin/next",
-        args: "start -p " + (process.env.PORT || 8080),
+        args: "start -p " + 8000,
         watch: false,
         autorestart: true,
       },
