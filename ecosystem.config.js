bc34d0ac05b7bd6ec7e0c84b645187f7966edc09
Faraz Ehsan <faraz.ahsaan@hotmail.com>
2024-01-10 18:29:00 +0500

    fixed system config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [
       {
-        name: "didik-medium",
+        name: "un-collab-portal-web",
         script: "./node_modules/next/dist/bin/next",
         args: "start -p " + (process.env.PORT || 8080),
         watch: false,
