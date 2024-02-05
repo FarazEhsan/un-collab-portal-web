c9e245e940d297de6fa42dde5041cf8a8e6da268
Faraz Ehsan <faraz.ahsaan@hotmail.com>
2024-02-05 14:10:17 +0500

    fixed admin page unwanted appearance in unauthorized access

--- src/hoc/role-hoc.tsx
+++ src/hoc/role-hoc.tsx
@@ -1,26 +1,31 @@
-import { useUser } from "@auth0/nextjs-auth0/client";
-import { useEffect } from 'react';
+import { useUser, withPageAuthRequired } from "@auth0/nextjs-auth0/client";
 
 export function withRole(PageComponent: any, roles: string[]) {
-  return function WithRoleComponent(props: any) {
+  const WithRoleComponent = (props: any) => {
     const { user, isLoading } = useUser();
 
-    useEffect(() => {
-      if (!isLoading && user) {
-        const userRoles: string[] = user['unhroles'] as string[];
-        const hasRole = roles.some(role => userRoles.includes(role));
-
-        if (!hasRole && typeof window !== 'undefined') {
-          window.location.href = '/unauthorized';
-        }
-      }
-    }, [isLoading, user]);
-
-    // Don't render the page component until the role check is complete
     if (isLoading) {
       return null; // or return a loading component
     }
 
+    //@ts-ignore
+    const userRoles: string[] = user['unhroles'] as string[];
+    const hasRole = roles.some(role => userRoles.includes(role));
+
+    if (!hasRole) {
+      if (typeof window !== 'undefined') {
+        window.location.href = '/unauthorized';
+      }
+      return null;
+    }
+
     return <PageComponent {...props} />;
   }
+
+  WithRoleComponent.getInitialProps = async (context:any) => {
+    const pageProps = (PageComponent.getInitialProps) ? await PageComponent.getInitialProps(context) : {};
+    return { ...pageProps };
+  };
+
+  return withPageAuthRequired(WithRoleComponent);
 }
